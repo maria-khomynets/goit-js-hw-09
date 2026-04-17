b9766a6cf60f1bd94e9bd6ea61c316d866e663cd
Maria Khomynets <masha.khomynets@gmail.com>
2026-04-17 14:54:48 +0300

    form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,20 +1,55 @@
 const form = document.querySelector('.feedback-form');
 const localStorageKey = 'feedback-form-state';
+
 let formData = {
   email: '',
   message: '',
 };
 
+// ЗБЕРЕЖЕННЯ ПРИ ВВЕДЕННІ
 form.addEventListener('input', handleInput);
+
 function handleInput(event) {
   formData[event.target.name] = event.target.value;
-  const data = JSON.stringify(formData);
-  localStorage.setItem(localStorageKey, data);
+
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
 }
+
+// ВІДНОВЛЕННЯ ПРИ ЗАВАНТАЖЕННІ
+window.addEventListener('load', dataStorage);
+
+function dataStorage() {
+  const savedData = localStorage.getItem(localStorageKey);
+
+  if (!savedData) return;
+
+  const parsedData = JSON.parse(savedData);
+
+  formData = parsedData;
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+// SUBMIT
 form.addEventListener('submit', handleSubmit);
+
 function handleSubmit(event) {
   event.preventDefault();
-  const message = localStorage.getItem(localStorageKey);
-  console.log(message);
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(localStorageKey);
+
+  formData = {
+    email: '',
+    message: '',
+  };
+
   form.reset();
 }
